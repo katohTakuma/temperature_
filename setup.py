f3d77f166a687f31526353d0564098d8d6436af2
Takuma Katou <s23c1037tg@s.chibakoudai.jp>
2025-01-05 21:04:32 +0900

    修正

--- setup.py
+++ setup.py
@@ -12,17 +12,19 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-    (os.path.join('share', package_name), glob('launch/*.launch.py'))
+        (os.path.join('share', package_name), glob('launch/*.launch.py')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='taku0225',
-    maintainer_email='s23c1037tg@s.chibakoudai.jp',
+    maintainer='your_name',
+    maintainer_email='your_email@example.com',
     description='a package for practice',
     license='BSD-3-Clause',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-             'temperature = mypkg.temperature:main',
+            'temperature = mypkg.temperature:main',
         ],
     },
+)
+
