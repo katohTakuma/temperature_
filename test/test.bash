018050bcab172e00bb72b8af24c9d8da50d2e761
Takuma Katou <s23c1037tg@s.chibakoudai.jp>
2025-01-22 03:58:08 +0900

    修 正

--- test/test.bash
+++ test/test.bash
@@ -16,19 +16,13 @@ colcon build
 
 source install/setup.bash
 
-test_count=3
+echo "テストを実行しています..."
 
-for i in $(seq 1 $test_count); do
-    echo "テスト $i 番目を実行しています..."
+timeout 10s ros2 run temperature_node temperature &
 
-    timeout 10s ros2 run temperature_node temperature &
+ros2 topic pub /temperature std_msgs/Float32 "{data: 26.0}" &
 
-    ros2 topic pub /temperature std_msgs/Float32 "{data: 26.0}" &
-
-    ros2 topic echo /temperature_warning --once
-
-    echo "テスト $i 番目が完了しました。"
-done
+ros2 topic echo /temperature_warning --once
 
 echo "すべてのテストが完了しました。"
 
